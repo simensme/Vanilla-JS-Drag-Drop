727d06442ac5418775c846934fd1abe4620de695
Simen Smehaugen <simdevgen@outlook.com>
2024-01-30 13:16:46 +0100

    fixed save to local storage error

--- script.js
+++ script.js
@@ -49,10 +49,6 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  // console.log('columnEl:', columnEl);
-  // console.log('column:', column);
-  // console.log('item:', item);
-  // console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
@@ -94,14 +90,34 @@ function updateDOM() {
   });
 
   // Run getSavedColumns only once, Update Local Storage
-
-
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
+
+// Allow arrays to reflect Drag and Drop items
+const rebuildArrays = () => {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++){
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++){
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++){
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++){
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
+};
 
 // When Item starts dragging
 const drag = e => {
   draggedItem = e.target;
-  console.log('draggedItem', draggedItem);
 };
 
 // Column allows for item to Drop
@@ -125,6 +141,7 @@ const drop = e => {
   // Add item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 };
 
 // On load
